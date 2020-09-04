21e02d43d873710fa25ba77a47e3fb80c20c738d
ajaykhandge <ajaykhandge88@gmail.com>
2020-09-04 13:31:35 +0530

    added profile 1

--- dj_quiz/dj_quiz/settings.py
+++ dj_quiz/dj_quiz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mvaaez_xfsmbl%$$@eakw=3-9!^b4c8=#4-@37($^8_@h)0)p*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dj-quiz-app.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['dj-quiz-app.herokuapp.com','localhost']
 
 
 # Application definition
